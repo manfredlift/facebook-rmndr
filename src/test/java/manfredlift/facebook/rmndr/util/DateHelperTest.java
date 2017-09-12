c4eab0b6a619f3f736d612e4cff5160d524c896f
manfredve <liiv.manfred@gmail.com>
2017-09-12 11:36:10 +0100

    DateHelper tests

---
+++ src/test/java/manfredlift/facebook/rmndr/util/DateHelperTest.java
@@ -0,0 +1,21 @@
+package manfredlift.facebook.rmndr.util;
+
+import manfredlift.facebook.rmndr.api.ReferenceTime;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+public class DateHelperTest {
+    @Test
+    public void testHelper_successPlus1() {
+        ReferenceTime referenceTime = DateHelper.referenceTimeFromMillis(1505211935893L, 1);
+        assertThat(referenceTime.getReferenceTime(), is(equalTo("2017-09-12T11:25:35.893+01:00")));
+    }
+
+    @Test
+    public void testHelper_successMinus7() {
+        ReferenceTime referenceTime = DateHelper.referenceTimeFromMillis(1505211935893L, -7);
+        assertThat(referenceTime.getReferenceTime(), is(equalTo("2017-09-12T03:25:35.893+01:00")));
+    }
+}
